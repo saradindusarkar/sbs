9f9e389c2f03efefc60971a144559edde3eb61f0
saradindusarkar <s_sarkar96@hotmail.com>
2025-03-23 14:25:55 -0400

    Delete migraines directory

--- migraines/dlog/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from dlog.models import Dlog
-
-class DlogForm(forms.ModelForm):
-    class Meta:
-        model = Dlog
-        exclude = ['log_date']  # Exclude the log_date field
-
-
